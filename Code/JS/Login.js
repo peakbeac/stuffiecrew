c10c005dc6e84e89ed6e155124b9ffed4f98b8c6
Peak Beac <beacompeak@gmail.com>
2025-01-16 17:47:52 -0700

    Moved files

--- Code/JS/Login.js
+++ Code/JS/Login.js
@@ -1,137 +1,34 @@
-let enteredUser;
-let enteredPass;
+let user;
+let pass;
 
+const tUser = t;
+const tPass = p;
 
-const uTUser = t;
-const uTPass = p;
+const pUser = peakbeac;
+const pPass = SuperSecretPassword;
 
+const dUser = danmanne;
+const dPass = Admin;
 
-const u1User = peakbeac;
-const u2User = mistaMoo;
-const u3User = lborgman;
-const u4User = smamilla;
-const u5User = afasano;
-const u6User = user6;
+const mUser = matthew;
+const mPass = Admin;
 
-const u1Pass = SuperSecretPassword;
-const u2Pass = ILoveKoal;
-const u3Pass = MauiDog;
-const u4Pass = code;
-const u5Pass = CharlieDogg;
-const u6Pass = u6;
+const eUser = bravestever;
+const ePass = lalala;
 
-function checkPassword()
+const aUser = ariafasano;
+const aPass = lmp;
+
+const User7 = user7;
+const Pass7 = user7;
+
+function checkPW()
 {
-    if (enteredUser == uTUser)
+    if (user = tUser)
     {
-        if (enteredPass == uTPass)
+        if (pass = tPass)
         {
-            prompt('Welcome, Tester.');
-            loadHTML('TestLandingPage.html');
-        }
-        else
-        {
-            prompt('Password is incorrect. Please check password and try again.', enteredPass);
-            checkPassword();
-        }
-    }   
-    else if (enteredUser == u2User)
-    {
-        if (enteredPass == u2Pass)
-        {
-            prompt('Welcome, Everest Beacom.');
-            loadHTML('EBeacLandingPage.html');
-        }
-        else
-        {
-            prompt('Password is incorrect. Please check password and try again.', enteredPass);
-            checkPassword();
+            prompt()
         }
     }
-    else if (enteredUser == u1User)
-    {
-        if (enteredPass == u1Pass)
-        {
-            prompt('Welcome, Peak Beacom.');
-            loadHTML('PBeacLandingPage.html');
-        }
-        else
-        {
-            prompt('Password is incorrect. Please check password and try again.', enteredPass);
-            checkPassword();
-        }
-    }       
-    else if (enteredUser == u3User)
-    {
-        if (enteredPass == u3Pass)
-        {
-            prompt('Welcome, Landon Borgman.');
-            loadHTML('LBorgLandingPage.html');
-        }
-        else
-        {
-            prompt('Password is incorrect. Please check password and try again.', enteredPass);
-            checkPassword();
-        }
-    }         
-    else if (enteredUser == u4User)
-    {
-        if (enteredPass == u4Pass)
-        {
-            prompt('Welcome, Suhrith Mamilla.');
-            loadHTML('SMamiLandingPage.html');
-        }
-        else
-        {
-            prompt('Password is incorrect. Please check password and try again.', enteredPass);
-            checkPassword();
-        }
-    }      
-    else if (enteredUser == u5User)
-    {
-        if (enteredPass == u5Pass)
-        {
-            prompt('Welcome, Aria Fasano.');
-            loadHTML('AFasaLandingPage.html');
-        }
-        else
-        {
-            prompt('Password is incorrect. Please check password and try again.', enteredPass);
-            checkPassword();
-        }
-    }      
-    else if (enteredUser == u6User)
-    {
-        if (enteredPass == u6Pass)
-        {
-            prompt('LEAVE. NOW.');
-        }
-        else
-        {
-            prompt('Password is incorrect. Please check password and try again.', enteredPass);
-            checkPassword();
-        }
-    }      
 }
-
-
-function loadHTML(script)
-{
-    const xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function()
-    {
-        if (this.readyState == 4 && this.status == 200)
-        {
-            document.getElementById("target").innerHTML = this.responseText;
-        }
-    };
-    xhr.open("GET", script, true);
-    xhr.send();
-}
-
-
-prompt('Enter your username', enteredUser);
-
-prompt('Enter your password', enteredPass);
-
-checkPassword();
