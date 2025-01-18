ec7b3d49ceb9cf60e45c43953be8343bff056420
Peak Beac <beacompeak@gmail.com>
2025-01-17 20:52:02 -0700

    Code ud and ts
    Updated code and started troubleshooting

--- Code/JS/Login.js
+++ Code/JS/Login.js
@@ -22,6 +22,9 @@ const ePass = lalala;
 const aUser = ariafasano;
 const aPass = lmp;
 
+const sUser = suhrithmamilla;
+const sPass = lalala;
+
 const User7 = user7;
 const Pass7 = user7;
 
@@ -31,7 +34,115 @@ function checkPW()
     {
         if (pass = tPass)
         {
-            prompt()
+            prompt("Welcome, tester");
+            openHTML("testLP.html");
+        }
+        else
+        {
+            prompt("Password incorrect. Please try again", pass);
+            checkPW();
+        }
+    }
+    else if (user = pUser)
+    {
+        if (pass = tPass)
+        {
+            prompt("Welcome, tester");
+            openHTML("peakLP.html");
+        }
+        else
+        {
+            prompt("Password incorrect. Please try again", pass);
+            checkPW();
+        }
+    }
+    else if (user = dUser)
+    {
+        if (pass = dPass)
+        {
+            prompt("Welcome, tester");
+            openHTML("danielLP.html");
+        }
+        else
+        {
+            prompt("Password incorrect. Please try again", pass);
+            checkPW();
+        }
+    }
+    else if (user = mUser)
+    {
+        if (pass = mPass)
+        {
+            prompt("Welcome, tester");
+            openHTML("matthewLP.html");
+        }
+        else
+        {
+            prompt("Password incorrect. Please try again", pass);
+            checkPW();
+        }
+    }
+    else if (user = aUser)
+    {
+        if (pass = aPass)
+        {
+            prompt("Welcome, tester");
+            openHTML("ariaLP.html");
+        }
+        else
+        {
+            prompt("Password incorrect. Please try again", pass);
+            checkPW();
+        }
+    }
+    else if (user = eUser)
+    {
+        if (pass = ePass)
+        {
+            prompt("Welcome, tester");
+            openHTML("everLP.html");
+        }
+        else
+        {
+            prompt("Password incorrect. Please try again", pass);
+            checkPW();
+        }
+    }
+    else if (user = sUser)
+    {
+        if (pass = sPass)
+        {
+            prompt("Welcome, tester");
+            openHTML("suhrithLP.html");
+        }
+        else
+        {
+            prompt("Password incorrect. Please try again", pass);
+            checkPW();
+        }
+    }
+    else if (user = User7)
+    {
+        if (pass = Pass7)
+        {
+            prompt("Welcome, tester");
+            openHTML("testLP.html");
+        }
+        else
+        {
+            prompt("Password incorrect. Please try again", pass);
+            checkPW();
         }
     }
 }
+
+openHTML(script)
+{
+    xhr.open(script);
+}
+
+prompt("Enter your username", user);
+
+prompt("Enter your password", pass);
+
+checkPW();
